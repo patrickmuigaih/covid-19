43c0eb05447f609ce73fc0e6835f58aa229d9dff
patrick <nderitupatrick4@gmail.com>
2020-04-12 22:24:46 +0300

    use actual beds as 95% of beds

--- src/estimator.py
+++ src/estimator.py
@@ -34,8 +34,9 @@ def severe_cases_by_time(infections):
     return int(infections*15/100)
 
 def hospital_beds_by_time(beds_count, infections):
-    available_beds = int(beds_count * 35/100)
-    return available_beds - infections
+    actual_available_beds = int(beds_count * 95/100)
+    covid_beds = int(actual_available_beds * 35/100)
+    return covid_beds - infections
 
 def icu_request_by_time(infections):
     return int(infections * 5/100)
