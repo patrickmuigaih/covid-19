bd13adf7f203f3f770dc190c71e76defa9218813
patrick <nderitupatrick4@gmail.com>
2020-04-14 22:18:51 +0300

    remove commented database settings

--- covid_estimator/settings.py
+++ covid_estimator/settings.py
@@ -74,15 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'covid_estimator.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': 'db.sqlite3',
-#     },
-# }
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=500)
 }
